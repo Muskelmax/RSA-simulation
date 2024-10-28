fcf8951ec24e39e4add0a5c213b9925797eee5a3
Muskelmax <maxbuhler50@gmail.com>
2024-10-28 21:15:25 +0100

    fix: fixed more spelling mistakes

--- client_1/src/App.jsx
+++ client_1/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       axios
-        .get('https://rsa-simulation.onrender.com//message')
+        .get('https://rsa-simulation.onrender.com/message')
         .then((response) => {
           // console.log(response.data.data[0].message)
           dispatch(resetMessages())
@@ -31,7 +31,7 @@ const App = () => {
           console.log(error)
         }))
       axios
-        .get('https://rsa-simulation.onrender.com//client/2')
+        .get('https://rsa-simulation.onrender.com/client/2')
         .then((response) => {
           dispatch(newPublic([response.data.Keys[0].publicN, response.data.Keys[0].publicE]))
         })
